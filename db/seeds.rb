6329c467e57dd6bb1c491e54393a75a4b744d3aa
rnovick13 <rnovick13@gmail.com>
2020-01-30 12:45:46 -0500

    first commit and migration and seed data

---
+++ db/seeds.rb
@@ -0,0 +1,61 @@
+stardust = Book.create(title: "Stardust", author: "Neil Gaiman", cover: "https://i.pinimg.com/originals/18/04/7a/18047a8eb286f0340fc21dfbe65bf2fb.jpg")
+
+americangod = Book.create(title: "American Gods", author: "Neil Gaiman", cover: "https://images-na.ssl-images-amazon.com/images/I/51Pb-OAREFL._SX327_BO1,204,203,200_.jpg")
+
+handmaid = Book.create(title: "The Handmaid's Tale", author: "Margaret Atwood", cover: "https://images-na.ssl-images-amazon.com/images/I/91Idmo2GtiL.jpg")
+
+jurassic = Book.create(title: "Jurassic Park", author: "Michael Crichton", cover: "http://www.michaelcrichton.com/wp-content/uploads/2014/12/book-jurassicpark.jpg")
+
+pan = Book.create(title: "Peter Pan", author: "J.M. Barrie", cover: "https://static.greatbigcanvas.com/images/singlecanvas_thick_none/corbis/cover-of-peter-pan-and-wendy-by-j-m-barrie,2233356.jpg?max=1000")
+
+vampireinterview = Book.create(title: "Interview with the Vampire", author: "Anne Rice", cover: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1380631642i/43763.jpg")
+
+joy = Book.create(title: "The Joy Luck Club", author: "Amy Tan", cover: "https://i0.wp.com/www.nationalbook.org/wp-content/uploads/2015/12/1989_The-Joy-Luck-Club-by-Amy-Tan.jpg?fit=280%2C420&ssl=1")
+
+stranger = Book.create(title: "The Stranger Beside Me", author: "Ann Rule", cover: "https://images-na.ssl-images-amazon.com/images/I/51CgK5tPG2L._SX308_BO1,204,203,200_.jpg")
+
+lestat = Book.create(title: "The Vampire Lestat", author: "Anne Rice", cover: "https://vignette.wikia.nocookie.net/vampirechroncles/images/a/a1/The_Vampire_Lestat_book_cover.jpg/revision/latest?cb=20160528183024")
+
+shining = Book.create(title: "The Shining", author: "Stephen King", cover: "https://images.penguinrandomhouse.com/cover/9780385121675")
+
+it = Book.create(title: "It", author: "Stephen King", cover: "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781501182099/it-9781501182099_hr.jpg")
+
+sematary = Book.create(title: "Pet Sematary", author: "Stephen King", cover: "https://i0.wp.com/www.tor.com/wp-content/uploads/2014/12/PetSematary12.jpg?resize=250%2C405&type=vertical")
+
+cujo = Book.create(title: "Cujo", author: "Stephen King", cover: "https://d3myrwj42s63no.cloudfront.net/300/045/116/135/1/0451161351.jpg")
+
+rutheregod = Book.create(title: "Are You There God? It's Me, Margaret", author: "Judy Blume", cover: "https://covers.openlibrary.org/b/id/6943479-M.jpg")
+
+coraline = Book.create(title: "Coraline", author: "Neil Gaiman", cover: "https://media.bloomsbury.com/rep/bj/9781408841754.jpg")
+
+littlewomen = Book.create(title: "Little Women", author: "Louisa May Alcott", cover: "https://images.penguinrandomhouse.com/cover/9781524704735")
+
+pride = Book.create(title: "Pride and Prejudice", author: "Jane Austen", cover: "https://cdn.shopify.com/s/files/1/0953/9884/products/Pride-And-Prejudice-Book-Cover-Locket-Necklace-keyring-silver-Bronze-tone-B0964.jpg?v=1558470191")
+
+sense = Book.create(title: "Sense and Sensibility", author: "Jane Austen", cover: "https://images.penguinrandomhouse.com/cover/9780375756733")
+
+persuasion = Book.create(title: "Persuasion", author: "Jane Austen", cover: "https://images.penguinrandomhouse.com/cover/9780307386854")
+
+dracula = Book.create(title: "Dracula", author: "Bram Stoker", cover: "https://kbimages1-a.akamaihd.net/b9bb2f61-d2dc-4647-b368-50126e8e6e89/353/569/90/False/dracula-585.jpg")
+
+frankenstein = Book.create(title: "Frankenstein", author: "Mary Shelley", cover: "https://images.penguinrandomhouse.com/cover/9780553212471")
+
+hp1 = Book.create(title: "Harry Potter and the Sorcerer's Stone", author: "J.K. Rowling", cover: "https://images-na.ssl-images-amazon.com/images/I/51HSkTKlauL._SX346_BO1,204,203,200_.jpg")
+
+twocity = Book.create(title: "A Tale of Two Cities", author: "Charles Dickens", cover: "https://images.penguinrandomhouse.com/cover/9780451530578")
+
+christmascarol = Book.create(title: "A Christmas Carol", author: "Charles Dickens", cover: "https://images-na.ssl-images-amazon.com/images/I/51%2BGDJ0svfL._SX359_BO1,204,203,200_.jpg")
+
+richasian = Book.create(title: "Crazy Rich Asians", author: "Kevin Kwan", cover: "https://images-na.ssl-images-amazon.com/images/I/7144OITcNML.jpg")
+
+richasian2 = Book.create(title: "China Rich Girlfriend", author: "Kevin Kwan", cover: "https://images.penguinrandomhouse.com/cover/9780385539098")
+
+richasian3 = Book.create(title: "Rich People Problems", author: "Kevin Kwan", cover: "https://images.penguinrandomhouse.com/cover/9780385542241")
+
+hate = Book.create(title: "The Hate U Give", author: "Angie Thomas", cover: "https://www.booktopia.com.au/http_coversbooktopiacomau/big/9781406372151/0000/the-hate-u-give.jpg")
+
+marschron = Book.create(title: "The Martian Chronicles", author: "Ray Bradbury", cover: "https://images-na.ssl-images-amazon.com/images/I/518WxaFw0rL._SX307_BO1,204,203,200_.jpg")
+
+illustrated = Book.create(title: "The Illustrated Man", author: "Ray Bradbury", cover: "https://images-na.ssl-images-amazon.com/images/I/51fRL1sEdJL.jpg")
+
+faren = Book.create(title: "Farenheit 451", author: "Ray Bradbury", cover: "https://images-na.ssl-images-amazon.com/images/I/71OFqSRFDgL.jpg")
